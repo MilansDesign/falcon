716cd44a5279433c30a299347b749176b8eccfc2
MilansDesign <47286186+MilansDesign@users.noreply.github.com>
2019-03-07 03:09:12 +0100

    Add files via upload

--- devhawk/js/flower.js
+++ devhawk/js/flower.js
@@ -1,97 +1,101 @@
 console.clear();
 
-var doc = document;
-var flower = doc.querySelector('.flower');
-var range = doc.querySelector('.range');
+var screen_width = window.innerWidth;
+if(screen_width >= 768) {
 
-var petalPartMarkup = '<div class="box"> \
-                    <div class="shape"></div> \
-                </div>';
-
-var maxParts = 13;
-var maxPetalsDef = 6;
-var maxPetals = maxPetalsDef;
-
-var partsFontStepDef = 25 / maxParts;
-var partsFontStep = partsFontStepDef;
-var huetStep = 150 / maxParts;
-
-createFlower ();
-
-function createFlower () {
+    var doc = document;
+    var flower = doc.querySelector('.flower');
+    var range = doc.querySelector('.range');
     
-        var angle = 360 / maxPetals;
+    var petalPartMarkup = '<div class="box"> \
+                        <div class="shape"></div> \
+                    </div>';
     
-    for (var i = 0; i < maxPetals; i++) {
-        var petal = createPetal(); 
-        var currAngle = angle * i + 'deg';
-        var transform = 'transform: rotateY(' + currAngle + ') rotateX(-30deg) translateZ(9vmin)';
+    var maxParts = 13;
+    var maxPetalsDef = 6;
+    var maxPetals = maxPetalsDef;
+    
+    var partsFontStepDef = 25 / maxParts;
+    var partsFontStep = partsFontStepDef;
+    var huetStep = 150 / maxParts;
+    
+    createFlower ();
+    
+    function createFlower () {
         
-        petal.setAttribute( 'style',transform);
+            var angle = 360 / maxPetals;
         
-        flower.appendChild( petal );
+        for (var i = 0; i < maxPetals; i++) {
+            var petal = createPetal(); 
+            var currAngle = angle * i + 'deg';
+            var transform = 'transform: rotateY(' + currAngle + ') rotateX(-30deg) translateZ(9vmin)';
+            
+            petal.setAttribute( 'style',transform);
+            
+            flower.appendChild( petal );
+        }
     }
-}
-
-function createPetal () {
     
-    var box = createBox ( null, 0);
+    function createPetal () {
+        
+        var box = createBox ( null, 0);
+        
+        var petal = doc.createElement('div');
+        petal.classList.add('petal');
+        
+        for (var i = 1; i <= maxParts; i++) {
+            newBox = createBox ( box, i );        
+            box = newBox;
+        } 
+        
+        petal.appendChild( box );
     
-    var petal = doc.createElement('div');
-    petal.classList.add('petal');
+        return petal;
+    }
     
-    for (var i = 1; i <= maxParts; i++) {
-        newBox = createBox ( box, i );        
-        box = newBox;
+    function createBox ( box, pos ) {
+        
+        var fontSize = partsFontStep * ( maxParts - pos ) + 'vmin';
+        var half = maxParts / 2;
+        
+        var bright = '50';
+        
+        if ( pos < half + 1 ) {
+            fontSize = partsFontStep * pos + 'vmin';
+        }
+        else {
+            bright = 10 + 40 / half * ( maxParts - pos );
+        }
+        
+        var color = 'hsl(' + huetStep * pos + ', 100%, ' + bright + '%)';
+        
+        // 1. Add shape
+        var newShape = doc.createElement('div');
+        newShape.classList.add( 'shape' );
+    
+        // 2. Create wrapper .box
+        var newBox = doc.createElement('div');
+        newBox.classList.add('box');  
+        
+        var boxStyle = [
+            'color: ' + color,
+            'font-size: ' + fontSize
+        ].join(';');
+        newBox.setAttribute('style', boxStyle);
+    
+        // 3. Add old box to new box
+        if ( box ) {
+            newBox.appendChild( box );
+        }
+        
+        // 4. Add shape to new box
+        newBox.appendChild( newShape );
+    
+        return newBox;
+    }
+    
+    function out ( content ) {
+        console.log( content );
     } 
     
-    petal.appendChild( box );
-
-    return petal;
-}
-
-function createBox ( box, pos ) {
-    
-    var fontSize = partsFontStep * ( maxParts - pos ) + 'vmin';
-    var half = maxParts / 2;
-    
-    var bright = '50';
-    
-    if ( pos < half + 1 ) {
-        fontSize = partsFontStep * pos + 'vmin';
-    }
-    else {
-        bright = 10 + 40 / half * ( maxParts - pos );
-    }
-    
-    var color = 'hsl(' + huetStep * pos + ', 100%, ' + bright + '%)';
-    
-    // 1. Add shape
-    var newShape = doc.createElement('div');
-    newShape.classList.add( 'shape' );
-
-    // 2. Create wrapper .box
-    var newBox = doc.createElement('div');
-    newBox.classList.add('box');  
-    
-    var boxStyle = [
-        'color: ' + color,
-        'font-size: ' + fontSize
-    ].join(';');
-    newBox.setAttribute('style', boxStyle);
-
-    // 3. Add old box to new box
-    if ( box ) {
-        newBox.appendChild( box );
-    }
-    
-    // 4. Add shape to new box
-    newBox.appendChild( newShape );
-
-    
-    return newBox;
-}
-
-function out ( content ) {
-    console.log( content );
 }
